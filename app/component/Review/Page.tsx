efa36111ab473d9cd25af2f8ef26daa5e4b045a9
Vincent <vincentkiathadi@gmail.com>
2026-04-09 09:33:28 +0800

    add: filter and border for review component

--- app/component/Review/Page.tsx
+++ app/component/Review/Page.tsx
@@ -2,10 +2,18 @@ import { memo } from 'react';
 
 const Page = () => {
   return (
-    <div>
+    <div className='flex gap-3 justify-center items-center border p-3 rounded-sm'>
         <div className='w-[60%]'>
-            <img src='./thumbnail.jpg' alt='thumbnail' className='w-[20px] h-[20px]' />
-            <span>165cm</span><span>65kg</span><span>88/78/110cm</span>
+            <div>
+                <div className='flex justify-start items-center gap-1'>
+                    <img src='./thumbnail.jpg' alt='thumbnail' className='w-[20px] h-[20px]' /><img src='./star.png' alt='verified' className='w-[20px] h-[20px] mx-1' />
+                </div>
+               
+                <div className='text-xs text-gray-500 flex gap-3'>
+                    <span>165cm</span><span>65kg</span><span>88/78/110cm</span>
+                </div>
+                <br></br>
+            </div>
             <div>This black kaftan is a wardrobe staple for me now</div>
             <p>November 2025</p>
         </div>
--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,10 +1,18 @@
 import { memo } from 'react';
 import Review from '../component/Review/Page';
+import Filter from '../component/Filter/Page';
 
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen p-5">
       <h2>All Review</h2>
+      <br></br>
+      <br></br>
+      <Filter />
+      <br></br>
+      <br></br>
+
+
       <Review />
     </div>
   );
