ab47e5831778ff43c514c1759e547dbe810d6eff
Vincent <vincentkiathadi@gmail.com>
2026-04-09 09:49:33 +0800

    add: product details, designer

--- app/component/Banner/Page.tsx
+++ app/component/Banner/Page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <div>
       <img src='./Dress1.webp' alt='Dress1' className='w-[40%]' />
-      <h1 className='text-4xl font-bold mt-4'>Black Kaftan With Embellishment</h1>
+
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,17 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       
       <Banner />
+      <h1 className='text-4xl font-bold mt-4'>Black Kaftan With Embellishment</h1>
       <Star />
+      <div>
+        Designer
+      </div>
+      <div className='flex flex-col items-start gap-2'>
+        Product Details
+        <div>Fabric</div>
+        <div>Fit</div>
+        <div>Size Guide</div>
+      </div>
       <Link href='/review' className="text-2xl text-blue-500 mt-4">View More</Link>
       <Review />
 
