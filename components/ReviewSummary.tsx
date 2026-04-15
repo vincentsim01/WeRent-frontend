4bc11f74b8797fe6ec792689b913b20516c455b6
ingenioust91 <ingecendrawatisalim@gmail.com>
2026-04-15 20:22:12 +0700

    feat: add ReviewSummary component

---
+++ components/ReviewSummary.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Stars from './Stars'
+import Link from 'next/link'
+
+type ReviewSum = {
+  productId : number,
+  productRating : number,
+  productName : string,
+  backButton : boolean
+}
+
+function ReviewSummary({productId, productRating, productName, backButton}:ReviewSum) {
+  return (
+    <div>
+        <div className='flex items-center'>
+          <Link href={`/product/${productId}`} className={`${backButton ? 'block' : 'hidden'}`}>
+            <svg className='size-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+          </Link>
+          
+          <p className='font-size-1'>Reviews</p>
+        </div>
+        
+
+        <section className='flex gap-2 items-center'>
+            <p className='font-size-3'>3.2</p>
+            <div className='flex'>
+                <Stars rating={productRating}/>
+            </div>
+        </section>
+        <p>Reviews for {productName}</p>
+    </div>
+  )
+}
+
+export default ReviewSummary
